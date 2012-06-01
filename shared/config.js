84c4eb70484ff35c5fc48acce4772b292606b7d3
fhdemo3@example.com <fhdemo3@example.com>
2012-06-01 12:53:05 +0000

    update file /shared/config.js

--- shared/config.js
+++ shared/config.js
@@ -2,7 +2,7 @@ var config = {
   tabData: {
     tab1: [
       '    <h2>MAY - SPECIAL OFFER</h2>',
-      '<strong>10% off ALL Topups</strong>',
+      '<h3>10% off ALL Topups</h3>',
       '(with appropriate discount code)',
       'Click through the other tabs for more information and tips.',
       'Check out <a href="http://docs.feedhenry.com/wiki/App_Anatomy" target="_blank">this link</a> for full details.'
